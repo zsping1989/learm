3d0ab017c6d644c9c2a8e9dbfae4943fb534a2c7
zhangshiping <214986304@qq.com>
2017-01-08 15:55:53 +0800

    angular学习

---
+++ bower_components/main/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zhangshiping on 2017/1/8.
+ */
+(function(window,angular){
+    //定义模块名称
+    var MODULE_NAME = 'Main';
+    //定义指令前缀
+    var DIRECTIVE_PREFIX = 'ftx';
+    var app =  angular.module('ng'+MODULE_NAME,[]);
+   // app.
+})(window,window.angular);
