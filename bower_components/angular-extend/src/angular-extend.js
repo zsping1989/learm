f89aa79a792eb023ab1de24b80bef57a6ed5bca8
shiping.zhang <shiping.zhang@1905.com>
2017-01-10 18:43:31 +0800

    测试

--- bower_components/angular-extend/src/angular-extend.js
+++ bower_components/angular-extend/src/angular-extend.js
@@ -64,6 +64,7 @@
             childrens_key : 'childrens', //子节点字段
             element_name : '', //表单节点名称
             label : [], //标签
+            margin_tree : false, //边界数结构
             primary_key : 'id', //主键
             parent_key : 'parent_id' //父级字段
 
@@ -75,7 +76,7 @@
             prefix = prefix+'';//字符串类型
             var result = {};
             datas.map(function(item){
-                if(typeof item[childrens_key]=="object" && item[childrens_key].length){
+                if(typeof item[childrens_key]=="object" && item[childrens_key].length && childrens_key !=='' ){
                     item[childrens_key]=keyBy(item[childrens_key],key,childrens_key,prefix);
                 }
                 result[prefix+item[key]] = item;
@@ -83,9 +84,18 @@
             return result;
         };
 
+        //边界树换成树状结构
+        var toTree = function(datas){
+            var result = {};
+            for(var key in datas){
+                //result[key] =
+            }
+
+        }
+
         return {
             restrict: 'A', //属性
-            //templateUrl : './bower_components/main/multilevel-move.html',
+            //templateUrl : './bower_components/angular-extend/src/multilevel-move.html',
             template:'<span ng-repeat="(key,value) in seleceLength">'+
                         '<label>{{config[\'label\'][key]}}</label>'+
                         '<select name="{{config[\'element_name\']}}"  ng-model="area[key]" ng-change="change(area[key],key)" >'+
@@ -123,7 +133,7 @@
                 //监听数据改变
                 $scope.$watch($attr[prefixKey('MultilevelMove')], function (value) {
                     //将主键设置成key标记
-                    $scope.datas = keyBy($scope.$eval($attr[prefixKey('MultilevelMove')]),$scope.config['value'],$scope.config['childrens_key']);
+                    $scope.datas = $scope.config['margin_tree'] ? toTree(keyBy($scope.$eval($attr[prefixKey('MultilevelMove')]),$scope.config['primary_key'],'')) : keyBy($scope.$eval($attr[prefixKey('MultilevelMove')]),$scope.config['value'],$scope.config['childrens_key']);
 
                     //选择的值
                     $scope.area = typeof $scope.area=='undefined' ? [] : $scope.area;
