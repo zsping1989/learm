ce30aee1e52e693cfa3936d5d5cf4d8e97fa3f73
shiping.zhang <shiping.zhang@1905.com>
2017-01-11 14:15:34 +0800

    多级联动

--- bower_components/angular-extend/src/angular-extend.js
+++ bower_components/angular-extend/src/angular-extend.js
@@ -37,7 +37,7 @@
             }
         }
         return result;
-    }
+    };
 
     /**
      * 命令拼接前缀
@@ -64,6 +64,7 @@
             childrens_key : 'childrens', //子节点字段
             element_name : '', //表单节点名称
             label : [], //标签
+            empty : [], //未选折时的文案
             margin_tree : false, //边界数结构
             primary_key : 'id', //主键
             parent_key : 'parent_id', //父级字段
@@ -81,13 +82,16 @@
         var keyBy = function (datas,key,childrens_key,prefix){
             prefix = prefix || 'id_';
             prefix = prefix+'';//字符串类型
-            var result = {};
-            datas.map(function(item){
+            childrens_key = typeof childrens_key=="undefined" ? '' : childrens_key;
+            var result = {},item;
+            for (var i in datas){
+                item = datas[i];
                 if(typeof item[childrens_key]=="object" && item[childrens_key].length && childrens_key !=='' ){
                     item[childrens_key]=keyBy(item[childrens_key],key,childrens_key,prefix);
                 }
                 result[prefix+item[key]] = item;
-            });
+            }
+
             return result;
         };
 
@@ -99,7 +103,7 @@
          * @param   {String}    children的字符串
          * @return  {Array}     数组
          */
-        function toTree(a, idStr, pidStr, chindrenStr){
+        var toTree = function (a, idStr, pidStr, chindrenStr){
             var r = [], hash = {}, id = idStr, pid = pidStr, children = chindrenStr, i = 0, j = 0, len = a.length;
             for(; i < len; i++){
                 hash[a[i][id]] = a[i];
@@ -116,69 +120,100 @@
             return r;
         }
 
+        var treeFirst = function(datas,value_key,chindrens_key,result){
+            //定义结果
+            if(typeof result == 'undefined'){
+                var result = [];
+            }
+
+            for(var i in datas){
+                if(typeof datas[i][value_key] == 'undefined'){
+                    return ;
+                }
+                result[result.length] = datas[i][value_key] + '';
+                if(typeof datas[i][chindrens_key] == "object"){
+                    treeFirst(datas[i][chindrens_key],value_key,chindrens_key,result);
+                }
+                break;
+            }
+            return result;
+        };
+
         return {
             restrict: 'A', //属性
             //templateUrl : './bower_components/angular-extend/src/multilevel-move.html',
             template:'<span ng-repeat="(key,value) in seleceLength">'+
-                        '<label>{{config[\'label\'][key]}}</label>'+
-                        '<select name="{{config[\'element_name\']}}"  ng-model="area[key]" ng-change="change(area[key],key)" >'+
-                            '<option value="">请选择</option>'+
-                            '<option ng-repeat="x in value" value="{{x[config[\'value\']]}}">{{x[config[\'show\']]}}</option>'+
-                        '</select>'+
-                    '</span>',
-            link: function ($scope,$element, $attr) {
+                   '    <label>{{config[\'label\'][key]}}</label>'+
+                   '    <select name="{{config[\'element_name\'] ? config[\'element_name\']+\'[]\' : \'\'}}" ng-model="area[key]" ng-change="change(area[key],key)" >'+
+                   '        <option value="">{{config[\'empty\'][key] || \'请选择\'}}</option>'+
+                   '        <option ng-repeat="x in value" value="{{x[config[\'value\']]}}">{{x[config[\'show\']]}}</option>'+
+                   '    </select>'+
+                   '</span>',
+            link: function (scope,element, attr) {
 
                 //用户自定义配置
-                var main_config = $scope.$eval($attr[prefixKey('MultilevelMoveConfig')]);
+                var main_config = scope.$eval(attr[prefixKey('MultilevelMoveConfig')]);
 
                 //现在使用配置
-                $scope.config = overCinfig(config,main_config);
+                scope.config = overCinfig(config,main_config);
+
+                //选中值
+                scope.area = typeof scope.$eval(attr['ngModel']) == "object" ? scope.$eval(attr['ngModel']) : [];
+
+                for (var i in scope.area){
+                    scope.area[i] = scope.area[i]+''; //类型转换
+                }
 
                 //改变值
-                $scope.change = function(value,select_index){
+                scope.change = function(value,select_index){
 
                     //当前操作数据
-                    var datas = $scope.datas;
+                    var datas = scope.datas;
 
                     for(var i=0;i<select_index;i++){
-                        datas = datas['id_'+$scope.area[i]][$scope.config['childrens_key']];
+                        datas = datas['id_'+scope.area[i]][scope.config['childrens_key']];
                     }
                     //选择存在子节点,添加子节点选项
-                    if(typeof datas['id_'+value]=='object' && typeof datas['id_'+value][$scope.config['childrens_key']]=="object"){
-                        $scope.seleceLength[select_index+1] = datas['id_'+value][$scope.config['childrens_key']];
+                    if(typeof datas['id_'+value]=='object' && typeof datas['id_'+value][scope.config['childrens_key']]=="object"){
+                        scope.seleceLength[select_index+1] = datas['id_'+value][scope.config['childrens_key']];
                         //不存在子节点,删除子节点选项
                     }else {
-                        $scope.seleceLength.splice(select_index+1,$scope.seleceLength.length-(select_index+1));
-                        $scope.area.splice(select_index+1,$scope.area.length-(select_index+1));
+                        scope.seleceLength.splice(select_index+1,scope.seleceLength.length-(select_index+1));
+                        scope.area.splice(select_index+1,scope.area.length-(select_index+1));
                     }
                 };
 
                 //监听数据改变
-                $scope.$watch($attr[prefixKey('MultilevelMove')], function (value) {
+                scope.$watch(attr[prefixKey('MultilevelMove')], function (value) {
+
                     //获取数据
-                    var datas = $scope.$eval($attr[prefixKey('MultilevelMove')]);
+                    var datas = scope.$eval(attr[prefixKey('MultilevelMove')]);
 
                     //非树状结构转换树状结构
-                    if($scope.config['margin_tree']){
-                        datas = toTree(datas,$scope.config['primary_key'],$scope.config['parent_key'], $scope.config['childrens_key']);
+                    if(scope.config['margin_tree']){
+                        datas = toTree(datas,scope.config['primary_key'],scope.config['parent_key'], scope.config['childrens_key']);
                     }
 
                     //将主键设置成key标记
-                    $scope.datas =  keyBy(datas,$scope.config['value'],$scope.config['childrens_key']);
+                    scope.datas =  keyBy(datas,scope.config['value'],scope.config['childrens_key']);
 
-                    //选择的值
-                    $scope.area = typeof $scope.area=='undefined' ? [] : $scope.area;
+                    //默认选中第一个
+                    if(scope.config['selected'] && !scope.area.length){
+                        scope.area = treeFirst(scope.datas,scope.config['value'],scope.config['childrens_key']);
+                    }
 
                     //默认显示第一级
-                    $scope.seleceLength = [$scope.datas];
+                    scope.seleceLength = [scope.datas];
 
                     //循环显示后面层级
-                    for (var i=0;i<$scope.area.length;i++){
-                        if(typeof $scope.seleceLength[i]['id_'+$scope.area[i]]=='undefined'){
-                            $scope.area.splice(i,$scope.area.length-i);
-                            continue;
+                    for (var i=0;i<scope.area.length;i++){
+                        if(typeof scope.seleceLength[i]=='undefined' || typeof scope.seleceLength[i]['id_'+scope.area[i]]=='undefined'){
+                            scope.area.splice(i,scope.area.length-i);
+                            break;
                         }
-                        $scope.seleceLength[i+1] = $scope.seleceLength[i]['id_'+$scope.area[i]][$scope.config['childrens_key']];
+                        if(typeof scope.seleceLength[i]['id_'+scope.area[i]][scope.config['childrens_key']]!='undefined'){
+                            scope.seleceLength[i+1] = scope.seleceLength[i]['id_'+scope.area[i]][scope.config['childrens_key']];
+                        }
                     }
                 });
 
